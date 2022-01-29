bda787291aae60a9d9d676275b3852dfa3379705
Gregory keenan <gfkeenan4@gmail.com>
2022-01-29 15:29:54 +0000

    Begining Project

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//Program written by Greg Keenan to learn/tinker with arrays in C++
+//Nothing fancy, just learning
+//On github to learn/get more familiar with using git
+
+#include <iostream>
+
+int main() 
+{
+	
+}
